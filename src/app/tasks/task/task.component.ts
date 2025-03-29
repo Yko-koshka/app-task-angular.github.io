5170f687cdf05344fb25b6b375c8b6e97e902770
Yko-koshka <legezaanna7@gmail.com>
2025-03-29 14:37:39 +0100

    feat(tasks): format dueDate using Angular date pipe

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DatePipe } from '@angular/common';
 import { Task } from './task.model';
 import { CardComponent } from '../../chared/card/card.component';
 
 @Component({
   selector: 'app-task',
-  imports: [CardComponent],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.scss',
 })
